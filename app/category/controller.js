5fcb0249b94af38ca677a9807341f6ddf98b481b
Asya'ri ‎ <asyari818@gmail.com>
2023-01-03 10:19:34 +0700

    Create Catagory

--- app/category/controller.js
+++ app/category/controller.js
@@ -1,9 +1,32 @@
+const Category = require("./model");
+
 module.exports = {
   index: async (req, res) => {
     try {
       res.render("admin/category/view_category");
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  viewCreate: async (req, res) => {
+    try {
+      res.render("admin/category/create");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  actionCreate: async (req, res) => {
+    try {
+      const { name } = req.body;
+
+      let category = await Category({ name });
+      await category.save();
+
+      res.redirect("/category");
+    } catch (err) {
+      console.log(err);
     }
   },
 };
