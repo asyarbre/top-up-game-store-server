0d1be49ea7324e7ebb078c9903deebec9426502c
Asya'ri ‎ <asyari818@gmail.com>
2023-01-11 11:12:33 +0700

    Install Cors

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+var cors = require('cors')
+ 
 
 const usersRouter = require('./app/users/router');
 const dashboardRouter = require('./app/dashboard/router');
@@ -20,6 +22,7 @@ const authRouter = require('./app/auth/router');
 
 var app = express();
 const URL = `/api/v1`;
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
