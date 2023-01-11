f5bbc29f69eb8cb47dd713fe9cc539768d95051e
Asya'ri ‎ <asyari818@gmail.com>
2023-01-11 08:35:01 +0700

    Create API Dashboard

--- app/player/controller.js
+++ app/player/controller.js
@@ -157,11 +157,43 @@ module.exports = {
       const history = await Transaction.findOne({_id: id})
 
       if(!history) return res.status(404).json({message: "History not found"})
-      
+
       res.status(200).json({data: history})
 
     } catch (err) {
       res.status(500).json({ message: err.message || "Internal Server Error" });
+    }
+  },
+
+  dashboard: async (req, res) => {
+    try {
+      const count = await Transaction.aggregate([
+        { $match: { player: req.player._id } },
+        {
+          $group: {
+            _id: "$category",
+            value: { $sum: "$value" },
+          }
+        }
+      ])
+
+      const category = await Category.find()
+      category.forEach(element => {
+        count.forEach(data => {
+          if (data._id.toString() === element._id.toString()) {
+            data.name = element.name
+          }
+        })
+      })
+
+      const history = await Transaction.find({player: req.player._id})
+      .populate("category")
+      .sort({'UpdatedAt': -1})
+
+      res.status(200).json({data: history, count: count})
+    } catch (err) {
+      res.status(500).json({ message: err.message || "Internal Server Error" });
+      
     }
   },
 };
