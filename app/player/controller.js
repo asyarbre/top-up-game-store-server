c7cf64aa9f2e497c6d77fc7e0be961e25198fedc
Asya'ri ‎ <asyari818@gmail.com>
2023-01-11 08:16:05 +0700

    Create API Detail History Transaksi

--- app/player/controller.js
+++ app/player/controller.js
@@ -150,4 +150,18 @@ module.exports = {
       res.status(200).json({ data: history, total: total.length ? total[0].value : 0 });
     } catch (err) {}
   },
+
+  historyDetail: async (req, res) => {
+    try {
+      const { id } = req.params
+      const history = await Transaction.findOne({_id: id})
+
+      if(!history) return res.status(404).json({message: "History not found"})
+      
+      res.status(200).json({data: history})
+
+    } catch (err) {
+      res.status(500).json({ message: err.message || "Internal Server Error" });
+    }
+  },
 };
